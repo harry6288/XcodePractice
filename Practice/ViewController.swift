663ccb0fe02e63e18db6f63f652362306ce112bb
hardeepkaur <hardeepwalia019@gmail.com>
2019-06-05 08:15:28 -0400

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by hardeep kaur on 2019-06-05.
+//  Copyright © 2019 hardeep kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
